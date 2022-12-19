fd4214f6a2ff9c38cfa00c4bb712fa2a5521cad7
Balázs Puzsár <puzsarbalazs200@gmail.com>
2022-12-19 14:01:50 +0100

    create header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HeaderComponent } from './components/header/header.component';
+import {ToolbarModule} from "primeng/toolbar";
+import {ButtonModule} from "primeng/button";
+import {BadgeModule} from "primeng/badge";
+import {OverlayPanelModule} from "primeng/overlaypanel";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+      BrowserAnimationsModule,
+        ToolbarModule,
+        ButtonModule,
+        BadgeModule,
+        OverlayPanelModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
