8bdbf8e94e4f1f3d0bd600bd0d0eee32e55b35c9
Balázs Puzsár <puzsarbalazs200@gmail.com>
2022-12-20 13:29:02 +0100

    Implementing categories filter, product-header, and product-box

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {HeaderComponent} from "./components/header/header.component";
+import {HomeComponent} from "./pages/home/home.component";
 
 
 const routes: Routes = [
-  //{path: "", component: HeaderComponent}
+  {path: "", redirectTo: "home", pathMatch:"full"},
+  {path: "home", component: HomeComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,21 +9,37 @@ import {ButtonModule} from "primeng/button";
 import {BadgeModule} from "primeng/badge";
 import {OverlayPanelModule} from "primeng/overlaypanel";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import { HomeComponent } from './pages/home/home.component';
+import {SidebarModule} from "primeng/sidebar";
+import {CardModule} from "primeng/card";
+import {AccordionModule} from "primeng/accordion";
+import { ProductsHeaderComponent } from './pages/home/components/products-header/products-header.component';
+import {MenuModule} from "primeng/menu";
+import { FiltersComponent } from './pages/home/components/filters/filters.component';
+import { ProductBoxComponent } from './pages/home/components/product-box/product-box.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
+    ProductsHeaderComponent,
+    FiltersComponent,
+    ProductBoxComponent
   ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-      BrowserAnimationsModule,
-        ToolbarModule,
-        ButtonModule,
-        BadgeModule,
-        OverlayPanelModule
-    ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ToolbarModule,
+    ButtonModule,
+    BadgeModule,
+    OverlayPanelModule,
+    SidebarModule,
+    CardModule,
+    AccordionModule,
+    MenuModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
